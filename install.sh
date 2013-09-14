d49f4f51889fb1d3dcf5e94c4f57a574387ec66c
jdiez17 <jose.manuel.diez@gmail.com>
2013-09-15 00:52:41 +0200

    Added i3 config

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ PWD=$(pwd)
 HOME=/home/$(whoami)
 
 ln -s $PWD/vimrc $HOME/.vimrc
-ln -s $PWD/vim $HOME/.vim
-
+ln -s $PWD/vim $HOME
+ln -s $PWD/awesome $HOME/.config
+ln -s $PWD/config/i3 $HOME/.config
