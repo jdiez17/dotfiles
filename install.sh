5109f540648f29aa721adb5c25c2e9fd84d1d1d3
jdiez17 <jose.manuel.diez@gmail.com>
2013-09-15 01:24:10 +0200

    Added i3status and ttytter

--- install.sh
+++ install.sh
@@ -7,3 +7,5 @@ ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/vim $HOME
 ln -s $PWD/awesome $HOME/.config
 ln -s $PWD/config/i3 $HOME/.config/i3
+ln -s $PWD/config/i3status $HOME/.config/i3status
+ln -s $PWD/ttytterrc $HOME/.config/.ttytterrc
